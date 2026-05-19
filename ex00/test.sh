141fa2fab313d1723d9b49f46a64686df5eabf32
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-19 17:30:22 -0300

    tests: adds assert str function

---
+++ ex00/test.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+assert_str_eq() {
+  if [ "$1" != "$2" ]; then
+    # Print error to stderr
+    echo "❌ FAIL: ${3:-Assertion failed}" >&2
+    echo "   Expected: '$1'" >&2
+    echo "   Actual:   '$2'" >&2
+    exit 1
+  else
+    echo "✅"
+  fi
+}
