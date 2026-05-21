b7d31e896b7f678f7b30ea34a9872e2207e44e6f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-21 15:33:36 -0300

    tests: it checks for test2 dir name

--- ex00/test.sh
+++ ex00/test.sh
@@ -226,6 +226,21 @@ test_should_guarantee_correct_timestamps_on_test2_file() {
   remove_test_files
 }
 
+test_should_guarantee_correct_name_for_test2_dir() {
+  create_test_files
+  if [ $? -eq 1 ]; then
+    return 1;
+  fi
+  cd test_files
+  echo $(get_nth_line "$(ls -l)" 4) | read permission hl o g bytes month day hour name
+  cd ..
+
+  expected_name=test2
+  assert_str_eq $expected_name $name
+
+  remove_test_files
+}
+
 test_should_guarantee_correct_permission_on_first_file
 test_should_guarantee_two_hard_links_on_first_file
 test_should_guarantee_correct_timestamp_on_first_file
