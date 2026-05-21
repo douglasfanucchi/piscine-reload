a23c277b3ec47dabbda7a86339403bce8b0cc959
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-05-21 15:55:14 -0300

    tests: it checks for test3 timestamp

--- ex00/test.sh
+++ ex00/test.sh
@@ -289,6 +289,29 @@ test_should_guarantee_correct_bytes_on_test3() {
   remove_test_files
 }
 
+test_should_guarantee_correct_timestamps_on_test3_file() {
+  create_test_files
+  if [ $? -eq 1 ]; then
+    return 1;
+  fi
+  cd test_files
+  echo $(get_nth_line "$(ls -l)" 5) | read permission hl o g bytes month day hour _
+  cd ..
+
+  expected_month=Jun
+  expected_day=1
+  expected_hour="23:44"
+  if [ $hour != $expected_hour ]; then
+    expected_hour=2025
+  fi
+
+  assert_str_eq $expected_month $month
+  assert_str_eq $expected_day $day
+  assert_str_eq $expected_hour $hour
+
+  remove_test_files
+}
+
 test_should_guarantee_correct_permission_on_first_file
 test_should_guarantee_two_hard_links_on_first_file
 test_should_guarantee_correct_timestamp_on_first_file
@@ -302,3 +325,4 @@ test_should_guarantee_correct_timestamps_on_test2_file
 test_should_guarantee_correct_permission_on_test3
 test_should_guarantee_correct_hardlinks_on_test3
 test_should_guarantee_correct_bytes_on_test3
+test_should_guarantee_correct_timestamps_on_test3_file
